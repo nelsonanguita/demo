ce93268c0117bcc6c0e980b83ddd96b0efec9ad1
nelsonanguita <F1rulaNS>
2022-06-06 21:43:32 -0400

    Inicio de sesion funcionando

--- navigation/BottomTab.js
+++ navigation/BottomTab.js
@@ -1,12 +1,12 @@
 import React, { Component, useState } from "react";
 import {StyleSheet, Text, View} from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { FaHome, FaUserEdit, FaList } from 'react-icons/fa';
 import Profile from '../screen/Profile';
 import Home from "../screen/Home";
 import Listas from "../screen/Listas";
 //import Addlist from "../screen/addList";
 //import listarrer from "../screen/listarrer";
+import Historial from "../screen/Historial";
 import Login from "../screen/Login";
 import Registro from "../screen/Registro";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
@@ -20,7 +20,7 @@ export const BottomTab = () =>{
             setUser(currentUser);
         })
     return(
-            <Tab.Navigator initialRouteName="Login"
+                     <Tab.Navigator  initialRouteName="Login"
             screenOptions={{
                 tabBarShowLabel:false,
                 tabBarStyle:{
@@ -30,7 +30,8 @@ export const BottomTab = () =>{
                     elevation: 0,
                     backgroundColor:'#ffffff',
                     borderRadius:15,
-                    height: 50,
+                    height: 60,
+                    width:380
 
                 }
             }} >
@@ -39,20 +40,66 @@ export const BottomTab = () =>{
                     <Tab.Group>
                         <Tab.Screen
                             name="Home"
-                            component = {Listas}//{Home}
+                            component = {Home}
                             options={{
                                 headerShown: false, //borrar encabezado
                                 tabBarIcon:({focused})=>(
                                     <View style={{
-                                        alignItems: 'center',justifyContent:'center',top:0
+                                        //alignItems: 'center',justifyContent:'center',top:0
                                     }}>
 
-                                    <Text>{auth.currentUser?.email}</Text>
+                                    <Text>Home</Text>
                                         </View>  
                                         )
                                         
                                     }}/> 
+                         <Tab.Screen
+                            name="Lista"
+                            component = {Listas}
+                            options={{
+                                headerShown: false, //borrar encabezado
+                                tabBarIcon:({focused})=>(
+                                    <View style={{
+                                       // alignItems: 'center',justifyContent:'center',top:0
+                                    }}>
+
+                                    <Text>Listas</Text>
+                                        </View>  
+                                        )
+                                        
+                                    }}/>  
+                                     <Tab.Screen
+                            name="Historial"
+                            component = {Historial}
+                            options={{
+                                headerShown: false, //borrar encabezado
+                                tabBarIcon:({focused})=>(
+                                    <View style={{
+                                      //  alignItems: 'center',justifyContent:'center',top:0
+                                    }}>
+
+                                    <Text>Historial</Text>
+                                        </View>  
+                                        )
+                                        
+                                    }}/>   
+                                     <Tab.Screen
+                            name="Perfil"
+                            component = {Profile}
+                            options={{
+                                headerShown: false, //borrar encabezado
+                                tabBarIcon:({focused})=>(
+                                    <View style={{
+                                       // alignItems: 'center',justifyContent:'center',top:0
+                                    }}>
+
+                                    <Text>Perfil</Text>
+                                        </View>  
+                                        )
+                                        
+                                    }}/>          
                     </Tab.Group>
+                    
                 ):(
                 <Tab.Group>
                     <Tab.Screen 
@@ -96,6 +143,7 @@ export const BottomTab = () =>{
                        
                 
             </Tab.Navigator>
+   
     );
 }
 const styles = StyleSheet.create({
--- screen/Listas.js
+++ screen/Listas.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import {Dimensions, SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar,TextInput } from 'react-native';
+import React, { useState }from 'react';
+import {Dimensions,Pressable,SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar,TextInput, Modal} from 'react-native';
 const { width, height } = Dimensions.get("window");
 
 const DATA = [
   {
     id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'Cervezqwqwqwqwa',
+    title: 'Cerveza',
     price: '$3500',
     cant: '3',
   },
@@ -24,21 +24,89 @@ const DATA = [
 ];
 
 
+
 const Item = ({ title }) => (
   <View style={styles.item}>
     <Text style={styles.title}>{title}</Text>
   </View>
 );
+
+const editItem = () =>{
+
+  <Modal
+  animationType="slide"
+  transparent={true}
+  visible={modalVisible}
+  onRequestClose={() => {
+    Alert.alert("Modal has been closed.");
+    setModalVisible(!modalVisible);
+  }}
+>
+  <View style={styles.centeredView}>
+    <View style={styles.modalView}>
+      <Text style={styles.modalText}>Hello World!</Text>
+      <Pressable
+        style={[styles.button, styles.buttonClose]}
+        onPress={() => setModalVisible(!modalVisible)}
+      >
+        <Text style={styles.textStyle}>Hide Modal</Text>
+      </Pressable>
+    </View>
+  </View>
+</Modal>
+}
+
 const App = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  
 
   const renderItem = ({ item }) => 
     //<Item title={item.title+" "+ item.price+" "+ item.cant} />
-    <View style={styles.item}>
-        <Text style={styles.producto}>{item.title}</Text> 
-        <Text style={styles.precio}>{item.price}</Text> 
-        <Text style={styles.cantidad}>{item.cant}</Text> 
-    </View>
+<View>
     
+<View>
+          
+          <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert("Modal has been closed.");
+            setModalVisible(!modalVisible);
+          }}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>Eliminar o Editar</Text>
+              
+              <TextInput style={styles.modalText}>Cerveza</TextInput>
+              <TextInput style={styles.modalText}>$2800</TextInput>
+              <TextInput style={styles.modalText}>2</TextInput>
+
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <Text style={styles.textStyle}>ACEPTAR</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
+        </View>
+        <View style={styles.item}>
+            <Text style={styles.producto}>{item.title}</Text> 
+            <Text style={styles.precio}>{item.price}</Text> 
+            <Text style={styles.cantidad}>{item.cant}</Text> 
+            <Pressable
+                style={[styles.button, styles.buttonOpen]}
+                onPress={() => setModalVisible(true)}
+                >
+              <Text style={styles.textStyle}>Editar</Text>
+          </Pressable>
+        </View>
+        
+</View>
     ;
 
   return (
@@ -72,32 +140,70 @@ const styles = StyleSheet.create({
     flexDirection: "row",
   },
   producto: {
-    fontSize: 15,
-    flex:1 ,
+    fontSize: 20,
     textAlign:'left',
-    //width:50,
-    flex:3
+    flex:3,
+    marginTop:5
 
   },
   precio: {
     fontSize: 20,
-    flex:1,
     textAlign:'center',
     marginRight:1,
     flex:2,
-    
+    marginTop:5    
   },
   cantidad: {
     fontSize: 20,
     flex:1,
     textAlign:'center',
-    //width:10,
-    flex:1
+    marginTop:5
   },
 
   container2:{
       marginTop:50
   },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  }
  
 });
 
